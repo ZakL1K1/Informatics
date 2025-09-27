206dcbf6b85ff48979cebd863b6107d8497b2c8f
ZakL1K1 <aleksandrovt161@gmail.com>
2025-09-28 00:07:56 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	float x, y;
+	cout << "Введите значение x и y:";
+	cin >> x >> y;
+	double PI = 3.1415926;
+
+	float r = ((cos(PI * x)) / (sin(PI * x))) - (1 / cos(y));
+	float s = tan(x) - log(abs(pow(x, 3) + pow(y, 3)));
+	cout << r << " " << s << "\n";
+
+	float c = max(r, s);
+	cout << c << "\n";
+
+	cout << "Введите Enter чтоб закончить программу...\n";
+	cin.get();
+}
