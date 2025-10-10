157edc0633f089ec22bb529882a03b06a4caac0a
ZakL1K1 <aleksandrovt161@gmail.com>
2025-10-10 17:30:14 +0300

    Add files via upload

---
+++ Lab_work/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main() {
+	// Создание окна
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Matrix");
+
+	// Размеры ячеек
+	const int cellSize = 50;
+	const int gridSize = 10;
+
+	// Масив для хранения ячеек
+	sf::RectangleShape cells[gridSize][gridSize];
+
+	for (int i = 0; i < gridSize; ++i){
+		for (int j = 0; j < gridSize; ++j){
+			cells[i][j].setSize(sf::Vector2f(cellSize - 2, cellSize - 2));
+			cells[i][j].setPosition(i * cellSize, j * cellSize);
+			cells[i][j].setFillColor(sf::Color::White);
+			cells[i][j].setOutlineColor(sf::Color::Black);
+			cells[i][j].setOutlineThickness(1);
+		}
+	}
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		window.clear(sf::Color::White);
+		for (int i = 0; i < gridSize; ++i) {
+			for (int j = 0; j < gridSize; ++j) {
+				if (i + j <= 4) {                          
+					cells[i][j].setFillColor(sf::Color::Green);
+				}
+				window.draw(cells[i][j]);
+			}
+		}
+		window.display();
+	}
+	return 0;
+}
